22744efd48acf3a64962cdb924e4ce0ad4039c03
Fahim-Hassan-Ovi <fahim.hassan.ovi@gmail.com>
2025-01-05 15:46:26 +0600

    Added the banner and our recipe section

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,8 +1,26 @@
 
 export default function Banner() {
-  return (
-    <div>
-      Banner
-    </div>
-  )
+    return (
+        <div className="bg-cover bg-no-repeat rounded-3xl mid:w-full h-full md:h-[35.5rem] mb-10" style={{
+            backgroundImage: 'url(https://i.ibb.co.com/yqPfxSb/chefs-cover.jpg)',
+        }}>
+            <div className="w-9/12 mx-auto flex flex-col justify-center items-center text-center h-full space-y-6 py-4">
+                <h1 className="font-bold text-white md:text-[3.25rem]">
+                    Discover an exceptional cooking class tailored for you!
+                </h1>
+                <p className="text-lg text-white font-normal">
+
+                    Unlock your culinary potential with a cooking class designed to match your tastes and skill level. Whether you`&apos;`re a beginner eager to learn the basics or an experienced cook looking to master new techniques, this personalized experience ensures you`&apos;`ll create delicious dishes with confidence. Join now and transform your kitchen into a hub of creativity and flavor!
+                </p>
+                <div className="flex gap-4 text-xl font-semibold">
+                    <button className="btn bg-green-400 rounded-full border-none">
+                        Explore now
+                    </button>
+                    <button className="btn btn-outline text-white hover:bg-green-400 rounded-full border-none">
+                        Our Feedback
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/Components/OurRecipe.jsx
+++ src/Components/OurRecipe.jsx
@@ -1,8 +1,11 @@
 
 export default function OurRecipe() {
-  return (
-    <div>
-      Our Recipes
-    </div>
-  )
+    return (
+        <div className="text-center my-8 w-8/12 mx-auto space-y-4">
+            <h1 className="font-semibold text-5xl text-gray-800">Our Recipe</h1>
+            <p className="text-base text-gray-600">
+                Explore our created recipes for culinary inspiration. From classic comfort foods to gourmet delights, we have got something to satisfy every craving. Elevate your cooking with our easy-to-follow recipes, perfect for any occasion.
+            </p>
+        </div>
+    )
 }
