fa48ded19753d9fa644c56ec3e39ee6d82ed93b4
Fahim-Hassan-Ovi <fahim.hassan.ovi@gmail.com>
2025-01-05 23:07:39 +0600

    added the want to cook table by clicking this cook btn

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,42 @@
 
-export default function Sidebar({recipeQueue}) {
-  return (
-    <div className="md:w-1/3">
-      Want to cook: {recipeQueue.length}
-    </div>
-  )
+export default function Sidebar({ recipeQueue }) {
+    return (
+        <div className="md:w-1/3 border-2 rounded-2xl text-gray-600 p-2 bg-base-100">
+            {/* Want to cook table */}
+            <div className="overflow-x-auto">
+                <h2 className="border-b-2 ax-auto font-semibold text-2xl text-gray text-center pb-2">
+                    Want To Cook: {recipeQueue.length}
+                </h2>
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Time</th>
+                            <th>Calories</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            recipeQueue.map((recipe, index) => (
+                                <tr key={index} className="hover">
+                                    <th>{index + 1}</th>
+                                    <td>{recipe.recipe_name}</td>
+                                    <td>{recipe.preparing_time}</td>
+                                    <td>{recipe.calories}
+                                        <td><button onClick={()=>console.log('active')} className=" bg-green-400 rounded-full px-2 py-1 md:px-4  my-2 text-gray-800 font-medium">Preparing</button></td>
+                                    </td>
+                                </tr>
+
+                            ))
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+            {/* Currently Cooking Table */}
+        </div>
+    )
 }
